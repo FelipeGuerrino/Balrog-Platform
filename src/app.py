b37f718c9f43a69bf5f3898de22b5dd95e53eee5
FelipeGuerrino <felipe_bg2005@hotmail.com>
2022-02-13 22:22:05 -0300

    config inicial do firebase + teste

--- src/app.py
+++ src/app.py
@@ -1,1 +1,23 @@
-import pyrebase
+import pyrebase, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+firebaseConfig={'apiKey': os.getenv('KEY'),
+    'authDomain': os.getenv('AUTH'),
+    'databaseURL': os.getenv('DB'),
+    'projectId': os.getenv('PROJECT'),
+    'storageBucket': os.getenv('BUCKET'),
+    'messagingSenderId': os.getenv('SENDER'),
+    'appId': os.getenv('APP'),
+    'measurementId': os.getenv('MEASUREMENT')}
+
+app=pyrebase.initialize_app(firebaseConfig)
+
+auth=app.auth()
+
+email=input("insira seu email: \n")
+senha=input("insira sua senha: \n")
+
+if(auth.sign_in_with_email_and_password(email, senha)):
+    print("logado com sucesso!")
