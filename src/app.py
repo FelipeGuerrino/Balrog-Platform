e6b1b8f0522a941497668f719153704be858a00a
FelipeGuerrino <felipe_bg2005@hotmail.com>
2022-02-13 21:57:18 -0300

    config inicial do firebase + teste

--- src/app.py
+++ src/app.py
@@ -1,1 +1,20 @@
 import pyrebase
+
+firebaseConfig={'apiKey': "AIzaSyA5oftsNguj-ppC1PaKvEO4Yiw1oMylSMY",
+    'authDomain': "balrogbot-10a80.firebaseapp.com",
+    'databaseURL': "https://www.balrogbot-10a80.firebase.io.com",
+    'projectId': "balrogbot-10a80",
+    'storageBucket': "balrogbot-10a80.appspot.com",
+    'messagingSenderId': "87745930958",
+    'appId': "1:87745930958:web:aad167518ed129623d529f",
+    'measurementId': "G-KMK6GTSM1P"}
+
+app=pyrebase.initialize_app(firebaseConfig)
+
+auth=app.auth()
+
+email=input("insira seu email: \n")
+senha=input("insira sua senha: \n")
+
+if(auth.sign_in_with_email_and_password(email, senha)):
+    print("logado com sucesso!")
